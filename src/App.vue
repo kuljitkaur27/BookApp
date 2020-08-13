8f4b3758e7a17835ca3a69f843d6de762ad47049
kuljitkaur27 <kuljitkaur1984@gmail.com>
2020-08-13 16:54:56 +1000

    Version 1.1

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <BookSearch msg="Welcome to Book Search"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import BookSearch from './components/BookSearch'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    BookSearch
   }
 }
 </script>
